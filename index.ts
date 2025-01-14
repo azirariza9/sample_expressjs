a277dd5d6294175060df10086e7cde03c20c1310
azirariza <azirariza9@gmail.com>
2025-01-14 12:06:29 +0700

    middle of editing index.ts

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { routeHello,routeAPINames,routeWeather } from './routes.js';
+import express, {Request,Response} from "express";
+
+const server = express();
+const port = 3000;
+
+server.get("/hello",function (_req: Request,res: Response){
+    const response = routeHello();
+    res.send(response);
+});
+
+server.get("/api/names", 
+    async function (_req: Request,res:Response):Promise<void> {
+    let response: string;
+    try {
+        response = await routeAPINames(req,res);
+    } catch(err) {
+        console.log (err);
+    }
+    res.send(response);
+});
+
+server.listen(port, function(){
+    console.log('Listening on '+ port);
+});
